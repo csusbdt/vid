8a5a3443d7777cc19cb20143282627d3253b5ef2
David Turner <csusbdt@gmail.com>
2017-12-11 16:11:00 -0800

    X

--- src3/Manager.cpp
+++ src3/Manager.cpp
@@ -6,11 +6,23 @@
 #include "globals.h"
 
 void Manager::activate() {
+	// Initialize frame to random hues.
 	for (int x = 0; x < preVideo.getPreWidth(); ++x) {
 		for (int y = 0; y < preVideo.getPreHeight(); ++y) {
 			preVideo.setPixel(x, y, Util::randomInt(0, 360));
 		}
-	}	
+	}
+
+	// Architecture:
+	//    The manager is the root object; it is created in main.
+	//    Objects other than the manager have a crontrolling, parent object
+	//    that activates and deactivates them.  Other than activation,
+	//    parents have no control over their children.
+	//    Typically, an object adds itself to the loop when activated
+	//    but this is not required.
+
+// doc generation Doxygen
+
 	loop.addObject(this);
 	circleScene.activate();
 }
